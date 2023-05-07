9175cca8de4886331449908271bfd1cb7c09b1c5
Stevan Milovanovic <stevanm993@gmail.com>
2023-05-07 02:16:49 +0200

    Use Downloadble Font

--- app/src/main/java/rs/smobile/chucknorrisjokes/ui/theme/Type.kt
+++ app/src/main/java/rs/smobile/chucknorrisjokes/ui/theme/Type.kt
@@ -4,11 +4,40 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import rs.smobile.chucknorrisjokes.R
+
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+private val fontName = GoogleFont("Lobster")
+
+private val fontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider)
+)
 
 val Typography = Typography(
+    headlineSmall = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.0.sp,
+        letterSpacing = 0.0.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.0.sp,
+        letterSpacing = 0.0.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
