3050a762fd5cba8c3d718d0fdaf3246a931d0e0d
Stevan Milovanovic <stevanm993@gmail.com>
2023-12-07 02:31:48 +0100

    Add Joke Categories

--- app/src/main/java/rs/smobile/chucknorrisjokes/analytics/AnalyticsConstants.kt
+++ app/src/main/java/rs/smobile/chucknorrisjokes/analytics/AnalyticsConstants.kt
@@ -6,13 +6,20 @@ object AnalyticsConstants {
 
     const val APP_CENTER_API_KEY = BuildConfig.APP_CENTER_API_KEY
 
+    const val JOKE_CATEGORIES_FETCHED = "joke_categories_fetched"
+    const val JOKE_CATEGORIES_FETCH_FAILED = "joke_categories_fetch_failed"
+    const val JOKE_CATEGORIES_FETCH_FAILED_MESSAGE = "joke_categories_fetch_failed_message"
+    const val JOKE_CATEGORIES_FETCH_EXECUTED = "joke_categories_fetch_executed"
+
     const val FETCH_INITIAL_JOKE = "fetch_initial_joke"
     const val FETCH_NEW_JOKE = "fetch_new_joke"
+    const val TOGGLE_JOKE_CATEGORY = "toggle_joke_category"
 
     const val NEW_JOKE_FETCHED = "new_joke_fetched"
     const val JOKE = "joke"
     const val JOKE_CREATED_AT = "joke_created_at"
     const val JOKE_CATEGORIES = "joke_categories"
+    const val TOGGLED_JOKE_CATEGORY = "toggled_joke_category"
 
     const val NEW_JOKE_FETCH_FAILED = "new_joke_fetch_failed"
     const val NEW_JOKE_FETCH_FAILURE_MESSAGE = "new_joke_fetch_failure_message"
--- app/src/main/java/rs/smobile/chucknorrisjokes/data/api/JokeApi.kt
+++ app/src/main/java/rs/smobile/chucknorrisjokes/data/api/JokeApi.kt
@@ -2,11 +2,17 @@ package rs.smobile.chucknorrisjokes.data.api
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import rs.smobile.chucknorrisjokes.data.api.model.Joke
 
 interface JokeApi {
 
     @GET(ApiConstants.JOKE_ENDPOINT)
-    suspend fun getJoke(): Response<Joke>
+    suspend fun getJoke(
+        @Query(ApiConstants.CATEGORY_QUERY_PARAM_NAME) category: String?
+    ): Response<Joke>
+
+    @GET(ApiConstants.JOKE_CATEGORY_ENDPOINT)
+    suspend fun getJokeCategories(): Response<List<String>>
 
 }
